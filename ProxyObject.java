9e872d8a2da2c0b08323890cca99d72e53b9c365
Chandler Griscom <cjgriscom@gmail.com>
2018-12-06 19:25:49 -0500

    Simplify instantiate primitives

--- ProxyObject.java
+++ ProxyObject.java
@@ -39,31 +39,7 @@ class ProxyObject implements InvocationHandler, Serializable {
 		for (String key : templateKeys()) {
 			ProxyDatatype dt = template.datatypes.get(key);
 			if (dt.isPrimitive() && dt.dimensions == 0) {
-				switch (dt.base) {
-				case Boolean:
-					fields.put(key, false);
-					break;
-				case Byte:
-					fields.put(key, (byte)0);
-					break;
-				case Double:
-					fields.put(key, 0.);
-					break;
-				case Float:
-					fields.put(key, 0f);
-					break;
-				case Integer:
-					fields.put(key, 0);
-					break;
-				case Long:
-					fields.put(key, 0L);
-					break;
-				case Short:
-					fields.put(key, (short)0);
-					break;
-				default:
-					break;
-				}
+				fields.put(key, dt.defaultValue);
 			}
 			
 		}
