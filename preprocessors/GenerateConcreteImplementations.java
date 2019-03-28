02164b12862bf1f4412050d29b82d530bbbc0a13
Chandler Griscom <cjgriscom@gmail.com>
2019-03-28 10:48:12 -0400

    Add stub for GenerateConcreteImplementations

---
+++ preprocessors/GenerateConcreteImplementations.java
@@ -0,0 +1,16 @@
+package com.xactmetal.abstraction.proxy.preprocessors;
+
+import javassist.ClassPool;
+import javassist.CtClass;
+import nl.topicus.plugins.maven.javassist.ClassTransformer;
+import nl.topicus.plugins.maven.javassist.TransformationException;
+
+public class GenerateConcreteImplementations extends ClassTransformer {
+
+	@Override
+	public void applyTransformations(ClassPool classPool, CtClass classToTransform) throws TransformationException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
